b089ea9deb87294c88fbbde122e25116c928b739
Aluno <fernandobf.ata@gmail.com>
2023-11-08 19:49:01 -0300

    comit inicial

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lacoRepeticao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //solicita uma tabuada para o usuário
+            string opcao = "";
+
+            while (opcao != "0") ;
+            {
+                
+
+                Console.WriteLine("Qual tqbuada deseja calcular?");
+                int tabuada = Convert.ToInt32(Console.ReadLine());
+
+                for(int num = 1; num <= 10; num++)
+                {
+                    Console.WriteLine($"{tabuada} x {num} = {tabuada * num}");
+                }
+            }
+            Console.WriteLine("Deseja imprimir outra tabuada? (0 = não)");
+            opcao = Console.ReadLine();
+            Console.Clear();
+
+
+        }
+    }
+}
